302619c8211e67c64cbbcf3d6a6d2aeaf0c280e4
Kristian Mandrup <kmandrup@gmail.com>
2009-12-10 12:28:27 +0100

    remove require puts in filters

--- lib/filters/include.rb
+++ lib/filters/include.rb
@@ -1,5 +1,3 @@
 require 'filters/filter'
 rfolders = Require.rfolder('filters', {:folders => ['simple', 'composite', 'list', 'msg_context', 'executor'], :root_files => :after, :exclude => 'include'})
 
-puts rfolders.inspect
-
--- lib/trace-util-adv.rb
+++ lib/trace-util-adv.rb
@@ -11,30 +11,3 @@ Require.rfolder('action_handler')
 Require.rfolder('targets')
 Require.rfolder('templates')
 Require.folder('trace_calls')
-
-
-# Module_filter = {
-#   :name => 'my modules',
-#   :default => :exclude,
-#   :module_rules => [{
-#     # id of modules rule set
-#     :name => ['my_modules'],
-#     :include => [/Hobo/],
-#     :exclude => [/Dryml/],
-#     :default => :exclude
-#   }]
-# }
-# 
-#   _filter = Module_filter
-# 
-#   context = {:modules => ['Blip', 'Blap']}.context
-#   
-#   puts context.inspect
-# 
-#   options = {:filters => _filter}    
-#   exec = Tracing::Filter::Executor.new(options)       
-#   result = exec.filters_allow?('msg', context)
-# 
-#   puts "Result:" + result.inspect  
-# 
-#   assert_equal false, result, "Filter should NOT allow passage"    
